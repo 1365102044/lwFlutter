28b14c831293afef0857e1af2664ffb75fb9b196
1365102044 <liu1365102044@163.com>
2019-06-02 12:00:57 +0800

    新增houseDeatilePage.dart

---
+++ lwflutterapp/lib/lwhooray/moudel/houseMoudel/lwHouseDeatilPage.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class lwHouseDeatilPage extends StatefulWidget {
+  @override
+  _lwHouseDeatilPageState createState() => _lwHouseDeatilPageState();
+}
+
+class _lwHouseDeatilPageState extends State<lwHouseDeatilPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
